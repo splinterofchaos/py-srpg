4996be4c9a49c9edf09735fde981cf29539b2eff
Scott Prager <splinterofchaos@gmail.com>
2020-08-17 16:02:34 -0400

    Remove unused shader vars.

--- shaders.cpp
+++ shaders.cpp
@@ -18,15 +18,13 @@ Error text_shader(GlProgram& tex_shader_program) {
   Shader frag(Shader::Type::FRAGMENT);
   frag.add_source(R"(
     #version 140
-    in vec2 TexCoord;
-    in vec4 Bg_color;
-    in vec4 Fg_color;
     out vec4 FragColor;
     uniform sampler2D tex;
     uniform vec4 bg_color;
     uniform vec4 fg_color;
     uniform vec2 top_left;
     uniform vec2 bottom_right;
+    in vec2 TexCoord;
     void main() {
       float a = 0;
       if (all(greaterThan(TexCoord, top_left)) && 
