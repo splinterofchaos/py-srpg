0f0d58d85cb1474c6c22c57b483859efa04d66dd
Scott Prager <splinterofchaos@gmail.com>
2020-08-23 20:31:05 -0400

    Represent mele attack as sequence of actions.
    This makes better use of our abstractions and makes its functionality
    more obvious.

--- action.cpp
+++ action.cpp
@@ -25,6 +25,10 @@ public:
     (sequence_.push_back(std::forward<Actions>(actions)), ...);
   }
 
+  void push_back(std::unique_ptr<Action> a) {
+    sequence_.push_back(std::move(a));
+  }
+
   void impl(Game& game, std::chrono::milliseconds dt,
             std::vector<std::function<void()>>& deferred_events) {
     if (current_ >= sequence_.size()) {
@@ -82,58 +86,31 @@ std::unique_ptr<Action> move_action(EntityId actor, Path path,
   return std::make_unique<MoveAction>(actor, path, change_final_position);
 }
 
-// TODO: This can just be a sequence.
-class MeleAction : public Action {
-  std::unique_ptr<Action> mover_;
-  std::unique_ptr<Action> attack_;
-  std::unique_ptr<Action> recoil_;
-
-  // Landing position after mover_.
-  glm::vec2 final_pos_;
-  EntityId attacker_;
-  EntityId defender_;
-
-  enum State { MOVING_TO_ATTACK_POSITION,
-               ATTACKING,
-               RETREATING } state_;
-
-public:
-  MeleAction(const Ecs& ecs, EntityId attacker, EntityId defender, Path path) {
-    // Move to target first. Otherwise, initiate the attack.
-    if (!path.empty()) {
-      final_pos_ = path.back();
-      mover_ = move_action(attacker, std::move(path));
-      state_ = MOVING_TO_ATTACK_POSITION;
-    } else {
-      state_ = ATTACKING;
-      final_pos_ = ecs.read_or_panic<GridPos>(attacker).pos;
-    }
-
-    attacker_ = attacker;
-    defender_ = defender;
-
-    glm::vec2 defender_pos = ecs.read_or_panic<GridPos>(defender).pos;
-    // Where the attacker will thrust towards.
-    glm::vec2 attack_pos = (defender_pos - final_pos_) * 0.3f + final_pos_;
-    attack_ = move_action(attacker, {final_pos_, attack_pos},
-                          false  /* change_final_position */);
-    recoil_ = move_action(attacker, {attack_pos, final_pos_},
-                          false  /* change_final_position */);
+std::unique_ptr<Action> mele_action(const Ecs& ecs, EntityId attacker,
+                                    EntityId defender, Path path) {
+  std::unique_ptr<Action> move_to_position;
+  glm::vec2 final_pos;
+  if (!path.empty()) {
+    final_pos = path.back();
+    move_to_position = move_action(attacker, std::move(path));
+  } else {
+    final_pos = ecs.read_or_panic<GridPos>(attacker).pos;
   }
 
-  void impl(Game& game, std::chrono::milliseconds dt,
-            std::vector<std::function<void()>>& deferred_events) override {
-    if (state_ == MOVING_TO_ATTACK_POSITION) {
-      mover_->run(game, dt, deferred_events);
-      if (mover_->finished()) state_ = ATTACKING;
-    } else if (state_ == ATTACKING) {
-      attack_->run(game, dt, deferred_events);
-      if (attack_->finished()) {
-        state_ = RETREATING;
+  glm::vec2 defender_pos = ecs.read_or_panic<GridPos>(defender).pos;
+  // Where the attacker will thrust towards.
+  glm::vec2 thrust_pos = (defender_pos - final_pos) * 0.3f + final_pos;
 
+  auto thrust = move_action(attacker, {final_pos, thrust_pos});
+  auto recoil = move_action(attacker, {thrust_pos, final_pos});
+
+  auto deal_damage = generic_action(
+      [attacker, defender]
+      (Game& game, float,
+       std::vector<std::function<void()>>& deferred_events) {
         Actor* defender_actor;
         GridPos* defender_pos;
-        if (game.ecs().read(defender_, &defender_actor, &defender_pos) !=
+        if (game.ecs().read(defender, &defender_actor, &defender_pos) !=
             EcsError::OK) {
           std::cerr << "Could not read defender's stats or position!"
                     << std::endl;
@@ -141,7 +118,7 @@ public:
         }
 
         Actor* attacker_actor;
-        if (game.ecs().read(attacker_, &attacker_actor) != EcsError::OK) {
+        if (game.ecs().read(attacker, &attacker_actor) != EcsError::OK) {
           std::cerr << "Could not read defender's stats!" << std::endl;
           return;
         }
@@ -175,29 +152,13 @@ public:
             auto seq = sequance_action(std::move(float_up), std::move(expire));
             game.ecs().write(damage_text, std::move(seq), Ecs::CREATE_ENTRY);
         });
-
       }
-    } else if (state_ == RETREATING) {
-      recoil_->run(game, dt, deferred_events);
-      if (recoil_->finished()) stop_short();
-    }
+  );
 
-    if (finished()) {
-      GridPos* grid_pos = nullptr;
-      Transform* transform = nullptr;
-      if (game.ecs().read(attacker_, &grid_pos, &transform) != EcsError::OK) {
-        std::cerr << "Could not read attacker's grid pos and transform!"
-                  << std::endl;
-        return;
-      }
-      grid_pos->pos = glm::round(final_pos_);
-      transform->pos = final_pos_;
-    }
-  }
-};
-
-std::unique_ptr<Action> mele_action(const Ecs& ecs, EntityId attacker,
-                                    EntityId defender, Path path) {
-  return std::make_unique<MeleAction>(ecs, attacker, defender,
-                                      std::move(path));
+  SequnceAction s;
+  if (move_to_position) s.push_back(std::move(move_to_position));
+  s.push_back(std::move(thrust));
+  s.push_back(std::move(deal_damage));
+  s.push_back(std::move(recoil));
+  return std::make_unique<SequnceAction>(std::move(s));
 }
