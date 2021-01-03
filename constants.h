e1c2c262f028fb23e4a11a7c37beed4d84feb932
Scott Prager <splinterofchaos@gmail.com>
2021-01-02 19:04:46 -0500

    Add missing header to repo: constants.h

---
+++ constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+constexpr inline float TILE_SIZE = 0.1f;
+constexpr float TEXT_SIZE = 0.07f;
+constexpr float TEXT_SCALE = TEXT_SIZE / TILE_SIZE;
+
+constexpr int WINDOW_HEIGHT = 800;
+constexpr int WINDOW_WIDTH = 800;
+
